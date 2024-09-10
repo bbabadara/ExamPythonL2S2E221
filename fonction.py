00eb0a0bb4341b3da4d1e43af98bd31c7f579cca
bbabadara <bbabadara@gmail.com>
2024-09-10 19:47:17 +0000

    faire rechere etudiant par telephone , nom, prenom, classe

--- fonction.py
+++ fonction.py
@@ -4,6 +4,13 @@ from Etudiant import Etudiant
 # Liste pour stocker les étudiants
 etudiants = []
 
+#insertion de donner sur le tableau d'etudiants pour la verifcation
+etudiants.append(Etudiant("Aissatou", "Sow", "771234567", "Classe A", 15.0, 16.0, 14.5))
+etudiants.append(Etudiant("Mamadou", "Diouf", "772345678", "Classe B", 12.5, 13.0, 11.0))
+etudiants.append(Etudiant("Fatoumata", "Diallo", "773456789", "Classe A", 18.0, 19.0, 17.5))
+etudiants.append(Etudiant("Ibrahima", "Ndiaye", "774567890", "Classe C", 10.0, 11.5, 12.0))
+etudiants.append(Etudiant("Mariama", "Gueye", "775678901", "Classe B", 14.0, 15.0, 13.5))
+
 #Fonction qui permet d'ajouter un etudiant
 def ajout_etudiant():
     verif=True
@@ -62,13 +69,13 @@ def est_telephone_existant(numero):
     return False    
 #Fontion qui affiche le tableau d'etudiant
 def affiche_tab_etudiants():
-    print("="*137)
+    print("-"*137)
     print(f"{'Prénom':<15} {'Nom':<15} {'Téléphone':<15} {'Classe':<15} {'Devoir':<15} {'Projet':<15} {'Examen':<15} {'Moyenne':<15}")
-    print("="*137)
+    print("-"*137)
     for etudiant in etudiants:
         moyenne=(etudiant.devoir + etudiant.projet + etudiant.examen) / 3
         print(f"{etudiant.prenom:<15} {etudiant.nom:<15} {etudiant.telephone:<15} {etudiant.classe:<15} {etudiant.devoir:<15} {etudiant.projet:<15} {etudiant.examen:<15} {moyenne:<32}")
-        print("="*137)
+        print("-"*137)
 
 #fonction qui affiche le menu
 def menu():
@@ -87,8 +94,7 @@ def menu():
         elif choix == '2':
             trier_etudiants_moyenne()
         elif choix == '3':
-            # rechercher_etudiant()
-            print("3!")
+            recherche_etudiant()
         elif choix == '4':
             # modifier_notes()
             print("5!")
@@ -104,4 +110,68 @@ def menu():
 def trier_etudiants_moyenne():
     etudiants.sort(key=lambda etudiant: etudiant.calculer_moyenne(), reverse=True)
     affiche_tab_etudiants()
+
+#fonction pour rechercher un étudiant par téléphone, nom, prénom ou classe
+def recherche_etudiant():
+    verif=True
+    while verif:
+        critere = input("Critère de recherche (t : Téléphone, n : Nom, p : Prénom, c : Classe): ")
+        
+        if critere == 't':
+            recherche_telephone()
+            verif=False
+        elif critere == 'n':
+            recherche_nom()
+            verif=False
+        elif critere == 'p':
+            recherche_prenom()
+            verif=False
+        elif critere == 'c':
+            recherche_classe()
+            verif=False
+        else:
+            print("Critère de recherche invalide.")
+   
+   
+#fonction pour rechercher un étudiant par téléphone
+def recherche_telephone():
+    telephone = input("Téléphone à rechercher: ")
+    while not est__numero_valide(telephone):
+            print("Numéro de téléphone invalide. Veuillez verifier le numéro")
+            telephone = input("Téléphone à rechercher: ")
+    for etudiant in etudiants:
+        if etudiant.telephone == telephone:
+            etudiant.afficher_etudiant()
+            return
+    print("Etudiant non trouvé.")
+    
+#fonction pour rechercher un étudiant par nom
+def recherche_nom():
+    nom = input("Nom à rechercher: ")
+    for etudiant in etudiants:
+        if etudiant.nom.lower() == nom.lower():
+            etudiant.afficher_etudiant()
+            return
+        print("Etudiant non trouvé.")
+
+#fonction pour rechercher un étudiant par prénom
+def recherche_prenom():
+    prenom = input("Prénom à rechercher: ")
+    for etudiant in etudiants:
+        if etudiant.prenom.lower() == prenom.lower():
+            etudiant.afficher_etudiant()
+            return
+        print("Etudiant non trouvé.")
+
+#fonction pour rechercher un étudiant par classe
+def recherche_classe():
+    classe = input("Classe à rechercher: ")
+    for etudiant in etudiants:
+        if etudiant.classe.lower() == classe.lower():
+            etudiant.afficher_etudiant()
+            return
+        print("Etudiant non trouvé.")
+            
+
+    
     
