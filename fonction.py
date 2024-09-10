b7c485f44065066424150c8fd72240e952194235
bbabadara <bbabadara@gmail.com>
2024-09-10 15:56:20 +0000

    possibilite d'ajouter plusieur etudiants

--- fonction.py
+++ fonction.py
@@ -6,33 +6,42 @@ etudiants = []
 
 #Fonction qui permet d'ajouter un etudiant
 def ajout_etudiant():
-    prenom = input("Prénom: ")
-    nom = input("Nom: ")
-    telephone = input("Téléphone: ")
-    while not est__numero_valide(telephone):
-        print("Numéro de téléphone invalide. Veuillez entrer un numéro au format 772641040")
+    verif=True
+    while verif:   
+        prenom = input("Prénom: ")
+        nom = input("Nom: ")
         telephone = input("Téléphone: ")
-    
-    classe = input("Classe: ")
-    
-    devoir = float(input("Note de devoir: "))
-    while not est_note_valide(devoir):
-        print("Les notes doivent être entre 0 et 20.")
-        devoir = float(input("Note de devoir: "))
-    
-    projet = float(input("Note de projet: "))
-    while not est_note_valide(projet):
-        print("Les notes doivent être entre 0 et 20.")
-        projet = float(input("Note de projet: "))
+        while not est__numero_valide(telephone):
+            print("Numéro de téléphone invalide. Veuillez entrer un numéro au format 772641040")
+            telephone = input("Téléphone: ")
         
-    examen = float(input("Note d'examen: "))
-    while not est_note_valide(examen):
-        print("Les notes doivent être entre 0 et 20.")
+        classe = input("Classe: ")
+        
+        devoir = float(input("Note de devoir: "))
+        while not est_note_valide(devoir):
+            print("Les notes doivent être entre 0 et 20.")
+            devoir = float(input("Note de devoir: "))
+        
+        projet = float(input("Note de projet: "))
+        while not est_note_valide(projet):
+            print("Les notes doivent être entre 0 et 20.")
+            projet = float(input("Note de projet: "))
+            
         examen = float(input("Note d'examen: "))
-    
-    etudiant = Etudiant(prenom, nom, telephone, classe, devoir, projet, examen)
-    etudiants.append(etudiant)
+        while not est_note_valide(examen):
+            print("Les notes doivent être entre 0 et 20.")
+            examen = float(input("Note d'examen: "))
+        
+        etudiant = Etudiant(prenom, nom, telephone, classe, devoir, projet, examen)
+        etudiants.append(etudiant)
+        choix=input("Voulez-vous ajouter un autre etudiant? (o/n): ")
+        if choix=="o":
+            verif=True
+        else:
+           verif=False
+           
 
+#fonction qui verifie si le numero est valide
 def est__numero_valide(numero):
     numero = numero.replace(" ", "")
     if len(numero) != 9:
@@ -41,17 +50,23 @@ def est__numero_valide(numero):
         return False
     return numero.isdigit()  
 
+#fonction qui verifie si la note est comprie entre 0 et 20
 def est_note_valide(note):
     return 0 <= note <= 20
 
-
+#fonction qui verifie si le numero ne se trouve pas deja dans le tableau d'etudiant
+def est_telephone_existant(numero):
+    for etudiant in etudiants:
+        if etudiant.telephone == numero:
+            return True
+    return False    
 #Fontion qui affiche le tableau d'etudiant
 def affiche_tab_etudiants():
-    print("="*120)
+    print("="*137)
     print(f"{'Prénom':<15} {'Nom':<15} {'Téléphone':<15} {'Classe':<15} {'Devoir':<15} {'Projet':<15} {'Examen':<15} {'Moyenne':<15}")
-    print("="*120)
+    print("="*137)
     for etudiant in etudiants:
         moyenne=(etudiant.devoir + etudiant.projet + etudiant.examen) / 3
-        print(f"{etudiant.prenom:<15} {etudiant.nom:<15} {etudiant.telephone:<15} {etudiant.classe:<15} {etudiant.devoir:<15} {etudiant.projet:<15} {etudiant.examen:<15} {moyenne:<15}")
-        print("="*120)
+        print(f"{etudiant.prenom:<15} {etudiant.nom:<15} {etudiant.telephone:<15} {etudiant.classe:<15} {etudiant.devoir:<15} {etudiant.projet:<15} {etudiant.examen:<15} {moyenne:<32}")
+        print("="*137)
         
